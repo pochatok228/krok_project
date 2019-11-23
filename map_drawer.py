30311d9ceae60a402b4c7f0b928562aee9242ebc
Daniel G <DanGSun@yandex.ru>
2019-11-23 15:37:10 +0300

    map working

--- map_drawer.py
+++ map_drawer.py
@@ -2,8 +2,8 @@ import folium
 import geopandas as gpd
 import pandas as pd
 
-from pandas.io.json import json_normalize
 from flask import Flask
+from shapely.geometry import *
 
 app = Flask(__name__)
 
@@ -13,8 +13,30 @@ gdf = gpd.read_file(fr)
 pdf = pd.read_json("photo_coordinates.json", orient="records")
 rdf = gpd.GeoDataFrame(pdf, geometry=gpd.points_from_xy(pdf.long, pdf.lat))
 
-# Initializing Choropleth
-map_data = folium.features.Choropleth(geo_data=gdf, )
+poly_data = {}
+
+for _, row in rdf.iterrows():
+    point: Point = row["geometry"]
+    for _, r2 in gdf.iterrows():
+        if point.within(r2.geometry):
+            if poly_data.get(r2.NAME):
+                if poly_data[r2.NAME][1]:
+                    poly_data[r2.NAME][1] += 1
+            else:
+                poly_data[r2.NAME] = []
+                poly_data[r2.NAME].append(r2.NAME)
+                poly_data[r2.NAME].append(1)
+            break
+
+
+poly_data = pd.DataFrame.from_dict(poly_data, orient="index", columns=["district", "num"])
+
+# Convert the GeoDataFrame to WGS84 coordinates
+map_data = folium.features.Choropleth(geo_data=gdf, data=poly_data, columns=["district", "num"],
+                                      key_on="feature.properties.NAME",
+                                      fill_color='YlOrRd',
+                                      fill_opacity=0.6,
+                                      legend_name='Кол-во результатов по запросу (точек): ')
 
 # Initializing Folium map
 mmap = folium.Map((55.75, 37.61), zoom_start=10)
